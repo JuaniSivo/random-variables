218d7c77fa4f449770f40a868558ec276d83b4c1
JuaniSivo <juan_ignacio_sivori@yahoo.com.ar>
2025-11-21 08:45:12 +0100

    added module distributions

--- pages_logic/generate_dist_page.py
+++ pages_logic/generate_dist_page.py
@@ -3,18 +3,7 @@ import pandas as pd
 
 from utils.plotting import show_distribution
 from utils.storage import load_saved, save_distribution
-
-from distributions.uniform_dist import uniform_ui, uniform_sample
-from distributions.gaussian_dist import gaussian_ui, gaussian_sample
-from distributions.lognormal_dist import lognormal_ui, lognormal_sample
-from distributions.triangular_dist import triangular_ui, triangular_sample
-
-DIST_UI_SAMPLE = {
-    "Uniform": (uniform_ui, uniform_sample),
-    "Gaussian": (gaussian_ui, gaussian_sample),
-    "Lognormal": (lognormal_ui, lognormal_sample),
-    "Triangular": (triangular_ui, triangular_sample)
-}
+from utils.distributions import DIST_UI_SAMPLE
 
 def generate_dist_page():
     # Load saved distributions
